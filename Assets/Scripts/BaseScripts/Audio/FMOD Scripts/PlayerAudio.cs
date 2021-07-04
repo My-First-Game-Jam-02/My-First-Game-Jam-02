b01f332688006502c10b083f41237ecb0f602f24
IvanDBeltran <IvanDBeltran@users.noreply.github.com>
2021-07-04 19:28:37 -0400

    Added Fmod

---
+++ Assets/Scripts/BaseScripts/Audio/FMOD Scripts/PlayerAudio.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] SSPlayerController m_PlayerController;
+    [SerializeField] bool m_IsPlayingSpiritSounds;
+
+    private void Awake()
+    {
+        if(m_PlayerController == null)
+        {
+            m_PlayerController = GetComponent<SSPlayerController>();
+        }
+    }
+
+    private void Update()
+    {
+        TriggerSpiritSounds();
+    }
+
+    private void TriggerSpiritSounds()
+    {   
+        if(m_PlayerController.isSpirit && !m_IsPlayingSpiritSounds)
+        {
+            Debug.Log("Play Spirit Sounds");
+            m_IsPlayingSpiritSounds = true;
+        }
+
+
+        if(!m_PlayerController.isSpirit && m_IsPlayingSpiritSounds)
+        {
+            m_IsPlayingSpiritSounds = false;
+        }
+
+       
+    }
+}
